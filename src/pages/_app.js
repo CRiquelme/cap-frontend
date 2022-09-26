5f46aa49786f6cc097587571e741382f493ea469
Pablo Abara <pbabara@uc.cl>
2022-09-26 12:11:16 -0300

    fix: yarn lint

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,9 +3,10 @@ import { OpenAPIProvider } from 'react-openapi-client';
 
 function MyApp({ Component, pageProps }) {
   return (
-  <OpenAPIProvider definition="http://localhost:3001/api-docs/index.html">
-    <Component {...pageProps} />
-  </OpenAPIProvider>);
+    <OpenAPIProvider definition="http://localhost:3001/api-docs/index.html">
+      <Component {...pageProps} />
+    </OpenAPIProvider>
+  );
 }
 
 export default MyApp;
