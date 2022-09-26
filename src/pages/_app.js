c0c477db7de4e2a4898ca3b96c0d445b84bad3d8
Pablo Abara <pbabara@uc.cl>
2022-09-26 11:45:40 -0300

    fix: rebase

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,10 @@
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+  <OpenAPIProvider definition="http://localhost:3000/api-docs/index.html">
+    <Component {...pageProps} />
+  </OpenAPIProvider>);
 }
 
 export default MyApp;
