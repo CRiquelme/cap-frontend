20ec2bbebdebbef6a36e4f2326b067b832bbc82b
Pablo Abara <pbabara@uc.cl>
2022-09-27 11:57:20 -0300

    feat: install prime and correct open-api-url

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,12 @@
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
 import styles from '@styles/Home.module.scss';
 import { OpenAPIProvider } from 'react-openapi-client';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <OpenAPIProvider definition="http://localhost:3001/api-docs/index.html">
+    <OpenAPIProvider definition="http://localhost:3001/api-docs/swagger.json">
       <Component className={styles.container} {...pageProps} />
     </OpenAPIProvider>
   );
