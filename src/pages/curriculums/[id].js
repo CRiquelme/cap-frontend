98d9d555b0d627fb18da2d946341146a86a6b9f6
Pablo Abara <pbabara@uc.cl>
2022-09-28 14:55:53 -0300

    fix: lint

--- src/pages/curriculums/[id].js
+++ src/pages/curriculums/[id].js
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react';
 import LearningUnitList from 'components/LearningUnitList';
 // import { LearningUnitService } from '../../service/LearningUnitService';
 import { useRouter } from 'next/router';
