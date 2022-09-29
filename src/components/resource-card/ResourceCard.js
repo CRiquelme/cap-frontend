ca4f105ac4a6011920b4db3cfefe1ba24052f38a
alejandroleiva <aleivailabaca@gmail.com>
2022-09-29 15:03:50 -0300

    create resource-card component

---
+++ src/components/resource-card/ResourceCard.js
@@ -0,0 +1,26 @@
+import { Panel } from 'primereact/panel';
+
+
+const ResourceCard = ({ resource }) => {
+
+  const template = (options) => {
+    const className = `${options.className} justify-content-start`;
+    const titleClassName = `${options.titleClassName} pl-1`;
+
+    return (
+        <div className={className}>
+            <span className={titleClassName}>
+                { options.name }
+            </span>
+        </div>
+    )
+}
+
+
+  return (
+    <Panel header={resource.name}>
+      <p id="resource-url">{ resource.url }</p>
+    </Panel>
+  )
+};
+export default ResourceCard;
