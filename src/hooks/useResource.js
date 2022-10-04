09e04d09e77ae4a579ed2d34391738ac863cc8fe
AlehandroL <100784837+AlehandroL@users.noreply.github.com>
2022-10-04 17:49:31 -0300

    Update useResource.js

--- src/hooks/useResource.js
+++ src/hooks/useResource.js
@@ -2,12 +2,14 @@ import useSWR from 'swr';
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-export default function useResource(id) {
+function useResource(id) {
   const { data, error } = useSWR(`http://localhost:3001/api/resources/${id}`, fetcher);
 
   return {
-    data: data,
+    data,
     isLoading: !error && !data,
     isError: error,
   };
 }
+
+export default useResource;
