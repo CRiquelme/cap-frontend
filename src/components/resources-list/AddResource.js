d858ace9ac7e0663319f3b00403c9700d86ffb52
Carlos Riquelme <criquelme2@gmail.com>
2022-10-04 11:49:14 -0300

    feat(resources-list): add yup (formik)

--- src/components/resources-list/AddResource.js
+++ src/components/resources-list/AddResource.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { InputText } from 'primereact/inputtext';
-import { Formik } from 'formik';
+import { Formik, Field, Form } from 'formik';
 import styles from '@styles/Modal.module.scss';
 
 const AddResource = () => {
@@ -8,16 +8,41 @@ const AddResource = () => {
   const [valueUrl, setValueUrl] = useState('');
 
   return (
-    <form>
-      <div>
+    <>
+      <Formik
+        initialValues={{
+          name: '',
+          url: '',
+        }}
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 500));
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+        <Form>
+          <div>
+            <h5>Nombre</h5>
+            <Field name="name" type="text" className={styles.pInputtext} autoFocus />
+          </div>
+          <div>
+            <h5>Url</h5>
+            <Field name="url" type="text" className={styles.pInputtext} />
+          </div>
+          {/* <button type="submit">Submit</button> */}
+        </Form>
+      </Formik>
+
+
+
+      {/* <div>
         <h5>Nombre</h5>
         <InputText className={styles.pInputtext} value={valueName} onChange={(e) => setValueName(e.target.value)} autoFocus />
       </div>
       <div>
         <h5>Url</h5>
         <InputText className={styles.pInputtext} value={valueUrl} onChange={(e) => setValueUrl(e.target.value)} />
-      </div>
-    </form>
+      </div> */}
+    </>
   )
 }
 
