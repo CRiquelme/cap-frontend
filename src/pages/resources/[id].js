ad9de2d18b1c8f966d7602762b07bf4d376ea087
alejandroleiva <aleivailabaca@gmail.com>
2022-10-04 16:11:03 -0300

    modify resource/[id] page to include evaluation data

--- src/pages/resources/[id].js
+++ src/pages/resources/[id].js
@@ -1,23 +1,32 @@
 import ResourcePanel from 'components/resource-panel/ResourcePanel';
 import { useRouter } from 'next/router';
+import useResourceEvaluation from '@hooks/useResourceEvaluation';
+import useResource from '@hooks/useResource';
+import useResourceAverageEvaluation from 'hooks/useResourceAverageEvaluation';
 
-import useResource from 'hooks/useResource';
-
-function ResourcePage() {
+const ResourcePage = () => {
   const router = useRouter();
   const resourceId = router.query.id;
 
-  const { resource, isLoading, isError } = useResource(resourceId);
+  const { data: resource, isLoading: isLoadingResource, isError: isErrorResource } = useResource(resourceId);
+  const { data, isLoading: isLoadingEvaluation, isError: isErrorEvaluation } = useResourceEvaluation(resourceId);
+  const { data: average_evaluation, isLoading: isLoadingAverage, isError: isErrorAverage, mutate } = useResourceAverageEvaluation(resourceId);
 
-  if (isLoading) {
+  if (isLoadingResource || isLoadingEvaluation || isLoadingAverage) {
     return 'loading';
   }
 
-  if (isError) {
+  if (isErrorResource || isErrorAverage) {
     return 'error';
   }
 
-  return <ResourcePanel resource={resource}></ResourcePanel>;
-}
+  let current_evaluation = undefined;
+
+  if (!isErrorEvaluation) {
+    current_evaluation = data.evaluation;
+  }
+
+  return <ResourcePanel resource={resource} current_evaluation={current_evaluation} current_average={average_evaluation.average_evaluation} reload_average={mutate}></ResourcePanel>;
+};
 
 export default ResourcePage;
