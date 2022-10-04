606e3b36e74b6df3a018f6620b7963ec27d9d49f
AlehandroL <100784837+AlehandroL@users.noreply.github.com>
2022-10-04 17:52:57 -0300

    Update LinkButton.js

--- src/components/resource-panel/LinkButton.js
+++ src/components/resource-panel/LinkButton.js
@@ -2,9 +2,8 @@ import { Button } from 'primereact/button';
 import Link from 'next/link';
 
 const LinkButton = ({ url }) => {
-  if (!url) {
-    return <></>;
-  }
+  if (!url) return <></>;
+  
   return (
     <p>
       <Link href={url}>
