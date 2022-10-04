081dd549470ce18d9f9dfcafda0e3b6e05ebb0a1
alejandroleiva <aleivailabaca@gmail.com>
2022-10-04 17:07:34 -0300

    formatting

--- src/components/resource-panel/LinkButton.js
+++ src/components/resource-panel/LinkButton.js
@@ -2,9 +2,8 @@ import { Button } from 'primereact/button';
 import Link from 'next/link';
 
 const LinkButton = ({ url }) => {
-
   if (!url) {
-    return <></>
+    return <></>;
   }
   return (
     <p>
