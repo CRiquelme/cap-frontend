cccf75a52737515bd5d6f145821d795697e681e9
alejandroleiva <aleivailabaca@gmail.com>
2022-10-04 16:12:07 -0300

    modify resource-panel component to be declarative

--- src/components/resource-panel/ResourcePanel.js
+++ src/components/resource-panel/ResourcePanel.js
@@ -1,9 +1,14 @@
 import { Panel } from 'primereact/panel';
+import AverageRating from './AverageRating';
+import RatingField from './RatingField';
+import LinkButton from './LinkButton';
 
-const ResourcePanel = ({ resource }) => {
+const ResourcePanel = ({ resource, current_evaluation, current_average, reload_average }) => {
   return (
     <Panel header={resource.name}>
-      <p id="resource-url">{resource.url}</p>
+      <AverageRating average={current_average} />
+      <RatingField current_evaluation={current_evaluation} resource_id={resource.id} reload_average={reload_average} />
+      <LinkButton url={resource.url} />
     </Panel>
   );
 };
