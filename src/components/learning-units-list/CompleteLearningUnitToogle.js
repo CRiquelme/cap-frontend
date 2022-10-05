16551494e9868e86cced4029ebd66ebb73057ba4
Pablo Abara <pbabara@uc.cl>
2022-10-05 18:04:38 -0300

    feat: refactor distributing logic from toggle completed button

--- src/components/learning-units-list/CompleteLearningUnitToogle.js
+++ src/components/learning-units-list/CompleteLearningUnitToogle.js
@@ -4,45 +4,11 @@ import { endpoints } from '@utils/endpoints';
 import useGet from '@hooks/useGet';
 import { Skeleton } from 'primereact/skeleton';
 
-export const CompleteLearningUnitToggle = ({ unit_id }) => {
-  const completedLearningUnitEndpoint = endpoints('isLearningUnitCompleted', unit_id);
-
-  const { data: isCompleted, isLoading, isError, mutate } = useGet(completedLearningUnitEndpoint);
-
-  if (isLoading) {
-    return <Skeleton shape="rectangle" width="50%" />;
-  }
-  if (isError) {
-    return 'error';
-  }
-
-  const handleOnChange = (clicked) => {
-    if (clicked.value) {
-      const requestOptions = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-      };
-      fetch(completedLearningUnitEndpoint, requestOptions).then((response) => {
-        if (response.ok) {
-          mutate(completedLearningUnitEndpoint);
-        }
-      });
-    } else if (!clicked.value) {
-      const requestOptions = {
-        method: 'DELETE',
-        headers: { 'Content-Type': 'application/json' },
-      };
-      fetch(completedLearningUnitEndpoint, requestOptions).then((response) => {
-        if (response.ok) {
-          mutate(completedLearningUnitEndpoint);
-        }
-      });
-    }
-  };
+export const CompleteLearningUnitToggle = ({ completed, onChangeHandler, completedLearningUnitEndpoint }) => {
 
   return (
     <div className={styles.checkbox}>
-      <ToggleButton onLabel="Completado" offLabel="No Completado" onIcon="pi pi-check" offIcon="pi pi-times" checked={isCompleted.completed} onChange={handleOnChange} />
+      <ToggleButton onLabel="Completado" offLabel="No Completado" onIcon="pi pi-check" offIcon="pi pi-times" checked={completed} onChange={onChangeHandler} />
     </div>
   );
 };
--- src/components/learning-units-list/LearningUnitListItem.js
+++ src/components/learning-units-list/LearningUnitListItem.js
@@ -4,8 +4,46 @@ import styles from '@styles/LearningUnitsList.module.scss';
 import profilePic from '@utils/images/unit.jpeg';
 import LinkButton from './LinkButton';
 import { CompleteLearningUnitToggle } from './CompleteLearningUnitToogle';
+import { endpoints } from '@utils/endpoints';
+import useGet from '@hooks/useGet';
+import { Skeleton } from 'primereact/skeleton';
 
 function LearningUnitItem({ unit }) {
+  const completedLearningUnitEndpoint = endpoints('isLearningUnitCompleted', unit.id);
+
+  const { data: isCompleted, isLoading, isError, mutate } = useGet(completedLearningUnitEndpoint);
+
+  const changeHandler = (clicked) => {
+    if (clicked.value) {
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+      };
+      fetch(completedLearningUnitEndpoint, requestOptions).then((response) => {
+        if (response.ok) {
+          mutate(completedLearningUnitEndpoint);
+        }
+      });
+    } else if (!clicked.value) {
+      const requestOptions = {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+      };
+      fetch(completedLearningUnitEndpoint, requestOptions).then((response) => {
+        if (response.ok) {
+          mutate(completedLearningUnitEndpoint);
+        }
+      });
+    }
+  };
+
+  if (isLoading) {
+    return <Skeleton shape="rectangle" width="50%" />;
+  }
+  if (isError) {
+    return 'error';
+  }
+
   return (
     <Card className={styles.cardFull}>
       <div className={styles.productListItem}>
@@ -13,7 +51,7 @@ function LearningUnitItem({ unit }) {
         <div className={styles.productListDetail}>
           <div className={styles.productName}>{unit.name}</div>
           <LinkButton url={`/learning-units/${unit.id}`} />
-          <CompleteLearningUnitToggle unit_id={unit.id} />
+          <CompleteLearningUnitToggle completed={isCompleted.completed} onChangeHandler={changeHandler} completedLearningUnitEndpoint={completedLearningUnitEndpoint} />
         </div>
       </div>
     </Card>
