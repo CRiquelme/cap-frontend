725c758b0478c3e3f497cbd50beeee94dcc931cc
Pablo Abara <pbabara@uc.cl>
2022-10-05 15:40:30 -0300

    refactor: added endpoints of resource page

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -4,9 +4,11 @@ export const endpoints = (operationId, param) => {
   const endpointsList = {
     curriculum: baseUrl + `/curriculums/${param}`,
     learningUnit: baseUrl + `/learning_units/${param}`,
+    resource: baseUrl + `//resources/${param}`,
     curriculumLearningUnits: baseUrl + `/curriculums/${param}/learning_units`,
     isLearningUnitCompleted: baseUrl + `/learning_units/${param}/completed`,
     learningUnitResources: baseUrl + `/learning_units/${param}/resources`,
+    resourceEvaluation: baseUrl + `resources/${param}/evaluation`,
     resourceAverage: baseUrl + `/resources/${param}/average_evaluation`,
   };
 
