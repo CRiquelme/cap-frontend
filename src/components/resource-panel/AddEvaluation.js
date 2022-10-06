9db673a2e3911b89fd10efeae3aa839c6cf18f6f
alejandroleiva <aleivailabaca@gmail.com>
2022-10-06 14:20:46 -0300

    add basic functionality to evaluate form

--- src/components/resource-panel/AddEvaluation.js
+++ src/components/resource-panel/AddEvaluation.js
@@ -3,21 +3,40 @@ import { InputTextarea } from 'primereact/inputtextarea';
 import { Rating } from 'primereact/rating';
 import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
+import { endpoints } from 'utils/endpoints';
 
-const AddEvaluation = () => {
-  const [evaluation, setEvaluation] = useState(undefined);
-  const [value, setValue] = useState('');
+const AddEvaluation = ({ myEvaluation }) => {
+  const [evaluation, setEvaluation] = useState(undefined)
+  const [comment, setComment] = useState("")
+  const [evaluated, setEvaluated] = useState(myEvaluation.hasEvaluated)
 
-  const handleErase = () => {};
-  const handleSubmit = () => {};
+  function handleErase() {
+    setEvaluation(undefined);
+    setComment("");
+  }
 
+  async function handleSubmit(comment) {
+    console.log(comment)
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ evaluation: evaluation, comment: comment }),
+    };
+    const response = await fetch(endpoints('resourceEvaluation', myEvaluation.resourceId), requestOptions);
+    await response.json();
+    myEvaluation.updateEvaluations();
+    myEvaluation.updateAverage();
+    setEvaluated(true);
+  }
+
+  if (evaluated) return <></>;
   return (
-    <Card title="Title" subTitle="SubTitle">
-      <Rating value={evaluation} onChange={(e) => setEvaluation(e.value)} cancel={false} />
-      <InputTextarea rows={5} cols={100} value={value} onChange={(e) => setValue(e.value)} autoResize maxlength="800" />
+    <Card title="Agregar comentario">
+      <Rating value={evaluation} onChange={(e) => setEvaluation(e.value)} cancel={false}/>
+      <InputTextarea rows={5} cols={100} value={comment} onChange={(e) => setComment(e.target.value) } autoResize maxlength="800" />
       <div className="dialog-demo">
         <Button type="button" label="Borrar" icon="pi pi-times" className="p-button-text" onClick={() => handleErase()} />
-        <Button type="submit" label="Guardar evaluación" icon="pi pi-check" onClick={() => handleSubmit()} />
+        <Button type="submit" label="Guardar evaluación" icon="pi pi-check" onClick={() => handleSubmit(comment)} />
       </div>
     </Card>
   );
