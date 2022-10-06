b355710577a4bcb61066f8c0435895fbfddbd1d9
Carlos Riquelme <criquelme2@gmail.com>
2022-10-06 12:34:43 -0300

    feat(curriculums-styles: add style to curriculums

--- src/components/learning-units-list/LearningUnitListItem.js
+++ src/components/learning-units-list/LearningUnitListItem.js
@@ -2,11 +2,12 @@ import { Card } from 'primereact/card';
 import Image from 'next/image';
 import styles from '@styles/LearningUnitsList.module.scss';
 import profilePic from '@utils/images/unit.jpeg';
-import LinkButton from './LinkButton';
+// import LinkButton from './LinkButton';
 import { CompleteLearningUnitToggle } from './CompleteLearningUnitToogle';
 import { endpoints } from '@utils/endpoints';
 import useGet from '@hooks/useGet';
 import { Skeleton } from 'primereact/skeleton';
+import Link from 'next/link';
 
 function LearningUnitItem({ unit }) {
   const completedLearningUnitEndpoint = endpoints('isLearningUnitCompleted', unit.id);
@@ -35,10 +36,18 @@ function LearningUnitItem({ unit }) {
   return (
     <Card className={styles.cardFull}>
       <div className={styles.productListItem}>
-        <Image className={styles.img} src={profilePic} alt={unit.name} />
+        <Link href={`/learning-units/${unit.id}`}>
+          <Image className={styles.img} src={profilePic} alt={unit.name} />
+        </Link>
         <div className={styles.productListDetail}>
-          <div className={styles.productName}>{unit.name}</div>
-          <LinkButton url={`/learning-units/${unit.id}`} />
+          <div className={styles.productName}>
+            <span>Learning unit</span>
+            <div>
+              <i className="pi pi-link">&nbsp;</i>
+              <Link href={`/learning-units/${unit.id}`}>{unit.name}</Link>
+            </div>
+          </div>
+          {/* <LinkButton url={`/learning-units/${unit.id}`} /> */}
           <CompleteLearningUnitToggle completed={isCompleted.completed} onChangeHandler={changeHandler} />
         </div>
       </div>
