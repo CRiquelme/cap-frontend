deca40df5a5463623e7d459f19bb51edbb10ab9d
alejandroleiva <aleivailabaca@gmail.com>
2022-10-07 10:57:07 -0300

    styling addEvaluation component

--- src/components/resource-section/AddEvaluation.js
+++ src/components/resource-section/AddEvaluation.js
@@ -3,6 +3,7 @@ import { InputTextarea } from 'primereact/inputtextarea';
 import { Rating } from 'primereact/rating';
 import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
+import styles from '@styles/ResourceEvaluations.module.scss';
 
 const AddEvaluation = ({ handleSubmit, defaultOptions }) => {
 
@@ -16,7 +17,7 @@ const AddEvaluation = ({ handleSubmit, defaultOptions }) => {
 
   return (
     <Card title={title}>
-      <Rating value={evaluation} onChange={(e) => setEvaluation(e.value)} cancel={false} readOnly={evaluated} />
+      <Rating value={evaluation} onChange={(e) => setEvaluation(e.value)} cancel={false} readOnly={evaluated} className={styles.inputRating} />
       <InputTextarea rows={4} cols={80} value={comment} onChange={(e) => setComment(e.target.value)} disabled={evaluated} autoResize maxlength="800" />
       <div className="dialog-demo">
         <Button type="button" label="Borrar" icon="pi pi-times" className="p-button-text" onClick={() => handleErase()} disabled={evaluated} />
