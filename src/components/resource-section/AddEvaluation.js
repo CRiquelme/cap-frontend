d7a8d49d6ea88dfeccf5c628f211fa84ee57cb67
alejandroleiva <aleivailabaca@gmail.com>
2022-10-07 11:11:25 -0300

    add toast when submmiting evaluation

--- src/components/resource-section/AddEvaluation.js
+++ src/components/resource-section/AddEvaluation.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { InputTextarea } from 'primereact/inputtextarea';
 import { Rating } from 'primereact/rating';
 import { Card } from 'primereact/card';
 import { Button } from 'primereact/button';
+import { Toast } from 'primereact/toast';
 import styles from '@styles/ResourceEvaluations.module.scss';
 
 const AddEvaluation = ({ handleSubmit, defaultOptions }) => {
@@ -12,8 +13,12 @@ const AddEvaluation = ({ handleSubmit, defaultOptions }) => {
   const [evaluated, setEvaluated] = useState(defaultOptions.evaluated);
 
   const handleErase = () => setComment('');
-
+  const toast = useRef(null);
   let title = evaluated ? "Tu evaluación" : "Agregar comentario";
+
+  function showSuccess() {
+    toast.current.show({severity: 'success', summary: 'Tu evaluación quedó registrada', detail: 'Gracias por contribuir!'});
+  }
 
   return (
     <Card title={title}>
@@ -21,8 +26,9 @@ const AddEvaluation = ({ handleSubmit, defaultOptions }) => {
       <InputTextarea rows={4} cols={80} value={comment} onChange={(e) => setComment(e.target.value)} disabled={evaluated} autoResize maxlength="800" />
       <div className="dialog-demo">
         <Button type="button" label="Borrar" icon="pi pi-times" className="p-button-text" onClick={() => handleErase()} disabled={evaluated} />
-        <Button type="submit" label="Guardar evaluación" icon="pi pi-check" onClick={() => {handleSubmit(evaluation, comment), setEvaluated(true)}} disabled={evaluated} />
+        <Button type="submit" label="Guardar evaluación" icon="pi pi-check" onClick={() => {handleSubmit(evaluation, comment), setEvaluated(true), showSuccess()}} disabled={evaluated} />
       </div>
+      <Toast ref={toast} position="bottom-center"/>
     </Card>
   );
 };
