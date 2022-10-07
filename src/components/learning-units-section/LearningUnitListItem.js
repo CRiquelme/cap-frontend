8e14b4fbc06386429a80676f3510991fbb5f0280
Pablo Abara <pbabara@uc.cl>
2022-10-07 11:20:24 -0300

    wip

--- src/components/learning-units-section/LearningUnitListItem.js
+++ src/components/learning-units-section/LearningUnitListItem.js
@@ -36,7 +36,15 @@ function LearningUnitItem({ unit }) {
     <Card className={styles.cardFull}>
       <div className={styles.productListItem}>
         <Link href={`/learning-units/${unit.id}`}>
-          <Image className={styles.img} src={profilePic} alt={unit.name} />
+          {/* <Image layout="fill" className={styles.img} src={profilePic} alt=""></Image> */}
+          <Image
+            layout="fill"
+            className={styles.img}
+            src={
+              "https://img.freepik.com/vector-gratis/mujeres-felices-aprendiendo-idioma-linea-aislado-ilustracion-vectorial-plana-personajes-femeninos-dibujos-animados-que-toman-lecciones-individuales-traves-messenger-concepto-educacion-tecnologia-digital_74855-10088.jpg?w=2000&t=st=1665151875~exp=1665152475~hmac=e788b587fbbe1bdd29332d55dc1a5965dd181962a0ee5719dc83ef09c895f0b4img.freepik.com/vector-gratis/mujeres-felices-aprendiendo-idioma-linea-aislado-ilustracion-vectorial-plana-personajes-femeninos-dibujos-animados-que-toman-lecciones-individuales-traves-messenger-concepto-educacion-tecnologia-digital_74855-10088.jpg?w=2000&t=st=1665151875~exp=1665152475~hmac=e788b587fbbe1bdd29332d55dc1a5965dd181962a0ee5719dc83ef09c895f0b4'"
+            }
+            alt={unit.name}
+          />
         </Link>
         <div className={styles.productListDetail}>
           <div className={styles.productName}>
