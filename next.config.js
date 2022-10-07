0bf9566e5bd8350e4cb148eff213701e730f6e9a
Pablo Abara <pbabara@uc.cl>
2022-10-07 10:56:45 -0300

    change landing page

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['fintualist.com', 'images.unsplash.com'],
+  },
   async rewrites() {
     return [
       {
